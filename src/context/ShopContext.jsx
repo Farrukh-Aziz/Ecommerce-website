1c5dfcdc63a9b6576f81b841b20999f61fbca0d9
Farrukh Aziz <mac@macs-MacBook-Air.local>
2025-05-14 18:42:38 +0500

    Add the fucntioanlity of the ADD TO CART button

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,19 +1,48 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 
 export  const ShopContext= createContext();
 
+
 const ShopContextProvider=(props)=>{
-     
     const currency = '$';
     const delivery_fee=10;
     const [search,setSearch]=useState('');
     const [showSearch,setShowSearch] = useState(false);
+    const [cartItem,setCartItem]=useState({});
+
+    const addToCart = async (itemId,size) =>{
+        
+         if (!size){
+            toast.error('Select product size')
+            return;
+         }
+
+          let cartData= structuredClone(cartItem);
+
+          if(cartData[itemId]){
+            if(cartData[itemId[size]]){
+                cartData[itemId][size] +=1;
+            }else{
+                cartData[itemId][size]=1;
+              }
+          }else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+        }
+        setCartItem(cartData);
+    }
+
+    useEffect(()=>{
+   console.log(cartItem)
+    },[cartItem])
+
 
     const value = {
         products,currency,delivery_fee,
-        search,setSearch,showSearch,setShowSearch
+        search,setSearch,showSearch,setShowSearch,cartItem,addToCart
     }
     return (
         <ShopContext.Provider value={value}>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
-import { assets, products } from "../assets/assets";
+import { assets } from "../assets/assets";
 import RelatedProduct from "../components/RelatedProduct";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency ,addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -80,7 +80,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5" />
