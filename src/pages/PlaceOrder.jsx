ed639b7bb3f1cc1ceb381a9961668072a0d1ec2f
Farrukh Aziz <mac@macs-MacBook-Air.local>
2025-05-16 17:07:07 +0500

    add place-order button and linked it with orders page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/cartTotal";
 import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
-  
-  const [methood,setMethood]=useState('cod')
+  const [methood, setMethood] = useState("cod");
+  const { navigate } = useContext(ShopContext);
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
-
-    {/* left side */}
+      {/* left side */}
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
         <div className="text-xl sm:text-2xl my-3">
           <Title text1={"DELIVERY"} text2={"DETAILS"} />
@@ -70,28 +70,58 @@ const PlaceOrder = () => {
       {/* Right side */}
       <div className="mt-8">
         <div className="mt-8 min-w-8">
-            <CartTotal/>
+          <CartTotal />
         </div>
 
         <div className="mt-12">
-          <Title text1={'PAYEMENT'} text2={'METHOOD'}/>
+          <Title text1={"PAYEMENT"} text2={"METHOOD"} />
           {/* PAYMENT METHOOD */}
           <div className="flex gap-3 flex-col lg:flex-row">
-            <div onClick={()=>setMethood('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className="min-w-3.5 h-3.5 border rounded-full"></p>
-                <img  className='h-5 mx-4' src={assets.stripe_logo}/> 
+            <div
+              onClick={() => setMethood("stripe")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  methood === "stripe" ? "bg-green-500" : ""
+                }`}
+              ></p>
+              <img className="h-5 mx-4" src={assets.stripe_logo} />
             </div>
-            <div onClick={()=>setMethood('razor')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className="min-w-3.5 h-3.5 border rounded-full"></p>
-                <img  className='h-5 mx-4' src={assets.razorpay_logo}/> 
+            <div
+              onClick={() => setMethood("razor")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  methood === "razor" ? "bg-green-500" : ""
+                }`}
+              ></p>
+              <img className="h-5 mx-4" src={assets.razorpay_logo} />
             </div>
-            <div onClick={()=>setMethood('cod')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className="min-w-3.5 h-3.5 border rounded-full"></p>
-                <p className="text-gray-500 text-sm font-medium mx-4">CASH ON DELIVERY</p>
+            <div
+              onClick={() => setMethood("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  methood === "cod" ? "bg-green-500" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
             </div>
           </div>
+          <div className="w-full text-end mt-8">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              PLACE ORDER
+            </button>
+          </div>
         </div>
-
       </div>
     </div>
   );
